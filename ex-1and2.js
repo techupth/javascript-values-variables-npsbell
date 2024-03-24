77cf0a3d488906cbaa1c33e3f42b4ff4af24edae
napasorn <napasorn.tsp@gmail.com>
2024-03-24 09:09:18 +0700

    submit: JavaScript Values Variables Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,29 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"; 
+
+const restaurantName = "Chef's Table by Chef James";
+const totalPriceCost = 15000;
+const totalNumberRestaurantCurrentlyOpen = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = '(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641';
+console.log(address)
+
+totalPriceCost = 5000;
+console.log(totalPriceCost)
+
+totalNumberRestaurantCurrentlyOpen = 2500;
+console.log(totalNumberRestaurantCurrentlyOpen)
+
+
+
+
+
+
+
+
+
+
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,5 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
 
-// Start coding here;
-
-let fahrenheit;
+let fahrenheit = celsius*1.8 + 32;
+console.log(fahrenheit)
